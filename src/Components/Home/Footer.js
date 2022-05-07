5f9de089de1d0672a415eaf9dd752326c75378b0
Shamsalfahad <shamsalfahad3@gmail.com>
2022-05-08 04:13:00 +0600

    footer added

--- src/Components/Home/Footer.js
+++ src/Components/Home/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="">
       <div className="bg-dark p-5 mt-3">
-        <h6 className="text-light">Copyright @ 2022 by | Cologne Gallery</h6>
+        <h6 className="text-light">Copyright @ 2022 by || Cologne Gallery</h6>
       </div>
     </div>
   );

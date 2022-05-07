b5d0e805f64fe5b8c6333a318486f01882464114
Shamsalfahad <shamsalfahad3@gmail.com>
2022-05-08 03:47:56 +0600

    Blogs added

--- src/Components/Blog/Blogs.js
+++ src/Components/Blog/Blogs.js
@@ -42,6 +42,17 @@ const Blogs = () => {
             on our website.
           </p>
         </div>
+        <div className="p-2 ">
+          <h4>When should you use node js and When should you use mongodb</h4>
+          <p>
+          <p>node js:</p>
+          Any project needs a programming environment and a runtime library that offers you basic programming tools/support and can compile and/or interpret your code. Nodejs is such as tool for the Javascript programming language. There are other similar tools for other languages such as Python, Java, PHP, C#, C++, Go, etc...
+
+So, if you want to write some kind of stand-alone program or server in Javascript, then you can use nodejs for it.
+          </p>
+          <p>MongoDB:</p>
+          <p>If your application needs the ability to persistently store data in a way that you can efficiently query or update it later, then you would typically use some form of database. There are dozens of popular databases. MongoDB is one such database. MariaDB, MySql, CouchDB, DynamoDB (on AWS), Postgres are examples of other databases. Different databases have different strengths (things they are best at) and different ways of using them so it's a whole different question to choose the right/best database for what you're doing.</p>
+        </div>
       </div>
     </div>
   );
